a5253b18c2f4f5615d6f7cc8f6fa2d225fcf372e
vleiswal <vleiswal@gmail.com>
2021-04-21 14:21:50 +0200

    not working

--- lib/EasyJsonParse/services.dart
+++ lib/EasyJsonParse/services.dart
@@ -9,12 +9,15 @@ class Services {
       'http://api.openweathermap.org/data/2.5/forecast?id=993800&appid=2c8c2dfe6b023b29c7ddebca0dea01f8';
 
   static Future<List<Weather>> getWeather() async {
+    print('Services... getWeather......');
     try {
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
         final List<Weather> cityWeather = weatherFromJson(response.body);
+        print(cityWeather);
         return cityWeather;
       } else {
+        print(response.statusCode);
         return List<Weather>();
       }
     } catch (e) {
